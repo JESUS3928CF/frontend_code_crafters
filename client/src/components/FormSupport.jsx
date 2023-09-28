24d4f3cd8bc5872584cd9f4602f603e8e6c69c10
jesus <jesus3928@gmail.com>
2023-09-28 09:56:22 -0500

    finalizado eL las peticiones al back del forlumario para filtrar apoyos

--- client/src/components/FormSupport.jsx
+++ client/src/components/FormSupport.jsx
@@ -7,6 +7,9 @@ const FormSupport = () => {
     const [institutions, setInstitutions] = useState([]);
     const [typeInstitutions, setTypeInstitutions] = useState([]);
     const [searchQuery, setSearchQuery] = useState('');
+    const [selectedInstitutionId, setSelectedInstitutionId] = useState(0);
+    const [selectedTypeInstitutionId, setSelectedTypeInstitutionId] =
+        useState(0);
 
     useEffect(() => {
         const fetchInstitutions = async () => {
@@ -36,6 +39,22 @@ const FormSupport = () => {
         fetchTypeInstitutions();
     }, []);
 
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        // Imprime por consola el id de la primera institución consultada.
+        console.log(
+            'ID de la primera institución consultada:',
+            institutions[0].id
+        );
+
+        // Imprime por consola el id del tipo de soporte seleccionado.
+        console.log(
+            'ID del tipo de soporte seleccionado:',
+            selectedTypeInstitutionId
+        );
+    };
+
     return (
         <>
             <NavBar />
@@ -43,53 +62,65 @@ const FormSupport = () => {
                 Buscar Apoyos
             </h3>
             <div className='flex justify-center items-center mt-10'>
-                <form className='bg-white py-10 px-5 md:w-1/3 rounded-xl shadow-xl border border-gray-600'>
+                <form
+                    className='bg-white py-10 px-5 md:w-1/3 rounded-xl shadow-xl border border-gray-600'
+                    onSubmit={handleSubmit}
+                >
                     <h3 className='text-center mb-3 text-xl font-bold'>
                         Te ayudaremos a buscar el apoyo que necesitas
                     </h3>
-                    <label
-                        htmlFor='nombre'
-                        className='text-gray-700 font-semibold text-base'
-                    >
-                        ¿A qué institución perteneces?
-                    </label>
-                    <input
-                        type='text'
-                        placeholder='Escribe aquí ...'
-                        className='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-lg mb-3 focus:outline-none focus:border-sky-700 focus:ring-sky-700'
-                        value={searchQuery}
-                        onChange={(e) => setSearchQuery(e.target.value)}
-                    />
 
-                    <p> Instituciones :</p>
-                    <ul className='list-disc mt-5 pl-5'>
-                        {institutions.map((institution) => (
-                            <li key={institution.id}>{institution.name}</li>
-                        ))}
-                    </ul>
+                    <div className='flex flex-col gap-y-5'>
+                        <label
+                            htmlFor='nombre'
+                            className='text-gray-700 font-semibold text-base'
+                        >
+                            ¿A qué institución perteneces?
+                        </label>
+                        <input
+                            type='text'
+                            placeholder='Escribe aquí ...'
+                            className='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-lg mb-1 focus:outline-none focus:border-sky-700 focus:ring-sky-700'
+                            value={searchQuery}
+                            onChange={(e) => setSearchQuery(e.target.value)}
+                        />
 
-                    <label
-                        htmlFor='nombre'
-                        className='text-gray-700 font-semibold text-base'
-                    >
-                        Tipo de apoyo
-                    </label>
-                    <select name='typeInstituteId' id='typeInstituteId'>
-                        {typeInstitutions.map((typeInstitution) => (
-                            <option
-                                key={typeInstitution.id}
-                                value={typeInstitution.id}
-                            >
-                                {typeInstitution.name}
-                            </option>
-                        ))}
-                    </select>
-                    
-                    <input
-                        type='submit'
-                        value='Buscar apoyo'
-                        className='bg-sky-600 p-3 mt-5 w-1/2 mx-auto block font-bold text-white rounded-xl cursor-pointer hover:bg-sky-700 transition-colors text-lg'
-                    />
+                        <p> Instituciones :</p>
+                        <ul className='list-disc mt-2 pl-5'>
+                            {institutions.map((institution) => (
+                                <li key={institution.id}>{institution.name}</li>
+                            ))}
+                        </ul>
+
+                        <label
+                            htmlFor='nombre'
+                            className='text-gray-700 font-semibold text-base'
+                        >
+                            Tipo de apoyo
+                        </label>
+                        <select
+                            name='typeInstituteId'
+                            id='typeInstituteId'
+                            onChange={(e) =>
+                                setSelectedTypeInstitutionId(e.target.value)
+                            }
+                        >
+                            {typeInstitutions.map((typeInstitution) => (
+                                <option
+                                    key={typeInstitution.id}
+                                    value={typeInstitution.id}
+                                >
+                                    {typeInstitution.name}
+                                </option>
+                            ))}
+                        </select>
+
+                        <input
+                            type='submit'
+                            value='Buscar apoyo'
+                            className='bg-sky-600 p-3 mt-5 w-1/2 mx-auto block font-bold text-white rounded-xl cursor-pointer hover:bg-sky-700 transition-colors text-lg'
+                        />
+                    </div>
                 </form>
             </div>
         </>
